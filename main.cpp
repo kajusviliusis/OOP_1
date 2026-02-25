4cca2c28a388f1a1c6970f25d32240e44920685c
kajusviliusis <kajus.viliusis0@gmail.com>
2026-02-25 19:36:29 +0200

    add header file with seperate cpp files

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#include "my_lib.h"
+
+
 #include <cctype>
 #include <iomanip>
 #include <iostream>
@@ -6,99 +9,6 @@
 #include <vector>
 #include <ctime>
 #include <cstdlib>
-
-struct Studentas{
-  std::string vardas;
-  std::string pavarde;
-  int egz;
-  std::vector<int> nd;
-};
-
-double vidurkis(const Studentas& s)
-{
-  if(s.nd.empty()) return 0;
-
-  double pazSuma = 0;
-  for(int i=0; i<s.nd.size(); i++)
-  {
-    pazSuma += s.nd[i];
-  }
-  return pazSuma / double(s.nd.size());
-}
-
-double mediana(const Studentas& s)
-{
-  if(s.nd.empty()) return 0;
-
-  std::vector<int> kopija = s.nd;
-  std::sort(kopija.begin(), kopija.end());
-
-  double med;
-  if(s.nd.size() % 2 == 0)
-  {
-    med = (kopija[s.nd.size() / 2 - 1] + kopija[s.nd.size() / 2]) / 2.0;
-  }
-  else {
-    med = kopija[s.nd.size() / 2];
-  }
-  return med;
-}
-
-double galutinis(const Studentas& A, double medVid)
-{
-  return 0.4*medVid + 0.6*A.egz;
-}
-
-void rodytiRezultatus(const std::vector<Studentas>& studentai)
-{
-  char skaiciavimas;
-  std::cout << "Ar skaičiuoti pagal vidurkį ar medianą? (v arba m)" << std::endl;
-  std::cin >> skaiciavimas; 
-
-  std::cout << std::left << std::setw(10) << "Vardas" << std::setw(15) << "Pavardė" << "Galutinis (" << (std::tolower(skaiciavimas) == 'm' ? "Med.)" : "Vid.)" ) << std::endl;
-
-  std::cout << "--------------------------------------------" << std::endl;
-  for(const Studentas& s : studentai)
-  {
-    double galutinisBalas;
-    if(std::tolower(skaiciavimas) == 'v') galutinisBalas = galutinis(s, vidurkis(s));
-    else galutinisBalas = galutinis(s, mediana(s));
-
-    std::cout << std::setw(10) << s.vardas << std::setw(15) << s.pavarde << std::setprecision(2) << std::fixed << galutinisBalas;
-    std::cout << std::endl;
-  }
-}
-
-void generuotiPazymius(Studentas& s)
-{
-  int kiekis = rand() % 15 + 1;
-  s.nd.clear();
-  for(int i=0; i<kiekis; i++)
-  {
-    s.nd.push_back(rand() % 10 + 1);
-  }
-
-  s.egz = rand() % 10 + 1;
-}
-
-void generuotiStudentus(std::vector<Studentas>& studentai)
-{
-  std::vector<std::string> vardai = {"Jonas", "Petras", "Antanas", "Marius", "Lukas", "Mantas", "Darius", "Andrius", "Tomas", "Linas"};
-  std::vector<std::string> pavardes = {"Kazlauskas", "Jankauskas", "Petrauskas", "Paukštis", "Stankevičius", "Vasiliauskas", "Žukauskas", "Butkus", "Paura", "Kairys"};
-
-  int studentuKiekis = rand() % 7 + 1;
-
-  for(int i=0; i<studentuKiekis; i++)
-  {
-    Studentas s;
-    s.vardas = vardai[rand() % vardai.size()];
-    s.pavarde = pavardes[rand() % pavardes.size()];
-
-    generuotiPazymius(s);
-    studentai.push_back(s);
-  }
-
-}
 
 int main()
 {
---
+++ my_lib.h
@@ -0,0 +1,21 @@
+#ifndef MYLIB_H
+#define MYLIB_H
+
+#include <string>
+#include <vector>
+
+struct Studentas {
+    std::string vardas;
+    std::string pavarde;
+    int egz;
+    std::vector<int> nd;
+};
+
+double vidurkis(const Studentas& s);
+double mediana(const Studentas& s);
+double galutinis(const Studentas& A, double medVid);
+void rodytiRezultatus(const std::vector<Studentas>& studentai);
+void generuotiPazymius(Studentas& s);
+void generuotiStudentus(std::vector<Studentas>& studentai);
+
+#endif
